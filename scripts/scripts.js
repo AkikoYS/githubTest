4d003ad4d72f88f8f901f59a13bb3fd5e96d17ae
{AkikoYS} <{akiko.yoshida@hotmail.co.jp}>
2022-01-16 14:22:57 +0900

    Second Commit

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+const animationRemote = bodymovin.loadAnimation({
+    container: document.getElementById('svg_00'),
+    path: 'https://lottiefiles.com/6818-a-chuckling-boy,
+    autoplay: true,
+    renderer: 'svg',
+    loop: true
+})
